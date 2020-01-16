6781d757d49fedeb12e2e36d89adaea0583a8a94
john connolly <jtconnolly82@gmail.com>
2020-01-16 11:23:20 -0700

    mongo connection string

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, "client", "build")))
 
 
 mongoose.set('useCreateIndex', true);
-mongoose.connect('mongodb://localhost:27017/reviewsdb',
+mongoose.connect(process.env.MONGO,
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
